5ca159a778a808e7d8aae61a20ce4709c789278b
Haris-Nr <buttharis34@gmail.com>
2024-01-24 18:43:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+    function changeTheme(e){
+        e.preventDefault()
+        const htmlTag = document.getElementsByTagName("html")[0]
+        
+        if (htmlTag.className.includes("dark")) {
+            htmlTag.className = 'light'
+        } else {
+            htmlTag.className = 'dark'
+        }
+    }
+
+    const switcher = document.getElementById("theme-mode")
+    switcher?.addEventListener("click" ,changeTheme )
+    
+    const chk = document.getElementById('chk');
+
+    chk.addEventListener('change',changeTheme);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      dai:['Dai Banna SIL','serif']
+      },
+    extend: {},
+
+  },
+  plugins: [],
+}
